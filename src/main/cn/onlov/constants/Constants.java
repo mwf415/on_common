570840f2b75872763504447d0521b94e6da982f1
mwf415 <mwf415@126.com>
2019-07-19 10:07:33 +0800

    aa

---
+++ src/main/cn/onlov/constants/Constants.java
@@ -0,0 +1,14 @@
+package cn.onlov.constants;
+
+public interface Constants {
+	Integer SYSTEM_MAIN_ID = 0;
+	Integer SYSTEM_EXAM_ID = 1;
+	Integer SYSTEM_CYCLE_ID = 2;
+	Integer SYSTEM_OSCE_ID = 3;
+	Integer SYSTEM_EVALUATE_ID = 4;
+	Integer THIS_SYSTEM_ID = 0;
+
+	Integer MENU_TYPE = 1;
+	Integer BUTTON_TYPE = 2;
+	String SALT = "youyicn";
+}
---
+++ src/main/cn/onlov/utils/MyPageUtil.java
@@ -0,0 +1,9 @@
+package cn.onlov.utils;
+
+public class MyPageUtil {
+
+    public static int currPage (int page,int pageSize){
+        return (page/pageSize)+1;
+
+    }
+}
